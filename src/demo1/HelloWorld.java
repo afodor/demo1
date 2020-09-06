9cb79e192224809bf75c683a6dd0831a9a4528e3
afodor <anthony.fodor@gmail.com>
2020-09-06 17:20:26 -0400

    it is now sunday afternoon

--- src/demo1/HelloWorld.java
+++ src/demo1/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello world!  It's Sunday!");
+		System.out.println("Hello world!  It's Sunday afternoon!");
 	}
 }
