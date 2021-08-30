09412c1d3c9fcc5f75803eb0aec38821e3e845d9
afodor <afodor@10.220.99.215>
2021-08-30 13:46:27 -0400

    in class

--- src/demo1/HelloWorld.java
+++ src/demo1/HelloWorld.java
@@ -5,5 +5,8 @@ public class HelloWorld
 	public static void main(String[] args)
 	{
 		System.out.println("Hello world!  It's Monday morning, really!");
+		System.out.println("Here is a second line");
+		System.out.println("Here is the third line");
+		
 	}
 }
