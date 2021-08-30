6174c992e3b17218e59e233718578aedef0041f7
afodor <afodor@10.220.99.215>
2021-08-30 13:53:32 -0400

    checking in

--- src/demo1/HelloWorld.java
+++ src/demo1/HelloWorld.java
@@ -4,6 +4,11 @@ public class HelloWorld
 {
 	public static void main(String[] args)
 	{
+		int x= 1;
+		
+		for (x=0; x < 10; x++)
+			System.out.println(x);
+		
 		System.out.println("Hello world!  I want a merge conflict!");
 		System.out.println("Here is a second line");
 		System.out.println("Here is the third line");
