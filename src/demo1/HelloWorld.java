27dba0918fb7da21f118253726e645c006d5e096
afodor <anthony.fodor@gmail.com>
2021-08-30 13:47:53 -0400

    Update HelloWorld.java
    merge conflict on purpose

--- src/demo1/HelloWorld.java
+++ src/demo1/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello world!  It's Monday morning, really!");
+		System.out.println("Hello world!  I want a merge conflict!");
 		System.out.println("Here is a second line");
 		System.out.println("Here is the third line");
 		
