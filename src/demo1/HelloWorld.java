c6544c70277a95d4dbf35933d3cc9b28ff7b7891
afodor <afodor@DESKTOP-T44GFRG.dyn.uncc.edu>
2023-08-23 11:38:31 -0400

    new classes

--- src/demo1/HelloWorld.java
+++ src/demo1/HelloWorld.java
@@ -4,18 +4,6 @@ public class HelloWorld
 {
 	public static void main(String[] args)
 	{
-		int x= 1;
-		
-		for (x=0; x < 10; x++)
-			System.out.println(x);
-		
-		System.out.println("Hello world!  I want a merge conflict!");
-		System.out.println("Here is a second line");
-		System.out.println("Here is the third line");
-		System.out.println("Here is a forth line");
-		
-		System.out.println("Here is a fifth line");
-		
-		
+		System.out.println("It is 2023");
 	}
 }
